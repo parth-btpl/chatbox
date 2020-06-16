b96175715c9c529f76aeea74cc099587d273694f
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-16 15:06:47 +0530

    create conversation model with recipient_id and sender_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_090032) do
+ActiveRecord::Schema.define(version: 2020_06_16_093231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id", unique: true
+    t.index ["sender_id"], name: "index_conversations_on_sender_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
