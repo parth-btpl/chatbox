f4a6c61490bcfb9ef3970fea4d06c3702301c2bf
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-16 17:05:15 +0530

    add conversation controller, add method to conversation model and controller to check session conversation is present?, else create new conversation, add routes and some view changes.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -7,6 +7,22 @@ class Conversation < ApplicationRecord
   # from rails guide - :scope option used to specify one or more attributes that are used to limit the uniqueness check
   validates :sender_id, uniqueness: { scope: :recipient_id }
 
+  # returns a conversation between two requested users
+  scope :between, -> (sender_id, recipient_id) do
+    where(sender_id: sender_id, recipient_id: recipient_id).or(
+      where(sender_id: recipient_id, recipient_id: sender_id)
+    )
+  end
+
+  # tries to get a conversation between two users – and if it’s present, it returns it.
+  # If there isn’t any, it creates a new one
+  def self.get(sender_id, recipient_id)
+    conversation = between(sender_id, recipient_id).first
+    return conversation if conversation.present?
+
+    create(sender_id: sender_id, recipient_id: recipient_id)
+  end
+
   # returns an opposed user to the requested user
   def opposed_user(user)
     user == recipient ? sender : recipient
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+
+  devise_for :users
+
+  resources :conversations, only: [:create]
 end
