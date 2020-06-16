e77e0c94263fe0badb8082153e8369b92ebab2c9
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-16 17:10:12 +0530

    add and remove line.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
   belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
   belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
-
 
   # from rails guide - :scope option used to specify one or more attributes that are used to limit the uniqueness check
   validates :sender_id, uniqueness: { scope: :recipient_id }
