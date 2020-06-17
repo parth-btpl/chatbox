137ad9af3c97db6941843b9d6a37d784b6e3df20
parth-btpl <parth.patel@botreetechnologies.com>
2020-06-17 11:43:20 +0530

    verify user to connect channel and create job.

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    protected
+    # Devise uses warden – it assigns to env[‘warden’] variable all information about the session and authentication.
+    # By calling a env[‘warden’].user, you can get a current user
+    def find_verified_user
+      if (current_user = env['warden'].user)
+        current_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
